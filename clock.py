90818fdddfc95ed6e3d01b67e7ab2512b0a1aad1
Michael Bechtel <m783b224@ku.edu>
2016-02-10 18:22:11 -0600

    Add option for a digital clock

--- clock.py
+++ clock.py
@@ -1,14 +1,15 @@
+import time
 import pygame, sys
 from pygame.locals import *
 import math
-import time
 import feed_HMS # our module
 import tick_sound # our mudule
 from decimal import Decimal
 
 
 HEIGHT = WIDTH = 400
-DISPLAY = 24
+DISPLAY = 12
+ANALOG = 0
 
 pygame.init()
 display = pygame.display.set_mode((HEIGHT,WIDTH),0,32)
@@ -42,7 +43,7 @@ punch = pygame.mixer.Sound('punch.wav')
 
 #choose sound
 sound = None
-sound = tick_sound.import_sound(slap,woosh,punch)
+#sound = tick_sound.import_sound(slap,woosh,punch)
 
 
 #need method to draw markers
@@ -55,47 +56,58 @@ while True:
 
 	#clear and fill the display
 	display.fill(WHITE)
+	if ANALOG == 0:
+                temp = time_input
+                seconds = temp % 60
+                minutes = time_input % 3600
+                temp = temp/3600
+                hours = temp
 
-        #draw the clock display
-	for x in range(0,60):
-            angle = x * ((2*math.pi)/60)
-            y_coor = (r*.9)*(-math.cos(angle))
-            x_coor = (r*.9)*(math.sin(angle))
+                time_string = font.render("%02d:%02d:%02d" % (hours, minutes, seconds), 1, BLACK)
+                display.blit(time_string, (center, center))
+                
 
-            #draw the numbers representing hours
-            if x % 5 == 0:
-                    if x == 0:
-                            #indicate that the time is between 0 and 12
-                            if DISPLAY == 24 and time_input < 43200:
-                                    number = font.render("12", 1, BLACK)
-                                    number2 = font.render("24", 1, RED)
-                            #indicate that the time is between 13 and 24
-                            elif DISPLAY == 24 and time_input >= 43200:
-                                    number = font.render("12", 1, RED)
-                                    number2 = font.render("24", 1, BLACK)
-                            #assume the user has it on a 12 hour cycle
-                            elif DISPLAY == 12:
-                                    number = font.render("12", 1, BLACK)
+	elif ANALOG == 1:
+                #draw the clock display
+                for x in range(0,60):
+                    angle = x * ((2*math.pi)/60)
+                    y_coor = (r*.9)*(-math.cos(angle))
+                    x_coor = (r*.9)*(math.sin(angle))
+
+                    #draw the numbers representing hours
+                    if x % 5 == 0:
+                            if x == 0:
+                                    #indicate that the time is between 0 and 12
+                                    if DISPLAY == 24 and time_input < 43200:
+                                            number = font.render("12", 1, BLACK)
+                                            number2 = font.render("24", 1, RED)
+                                    #indicate that the time is between 13 and 24
+                                    elif DISPLAY == 24 and time_input >= 43200:
+                                            number = font.render("12", 1, RED)
+                                            number2 = font.render("24", 1, BLACK)
+                                    #assume the user has it on a 12 hour cycle
+                                    elif DISPLAY == 12:
+                                            number = font.render("12", 1, BLACK)
+                            else:
+                                    #indicate that the time is between 0 and 12
+                                    if DISPLAY == 24 and time_input < 43200:
+                                            number = font.render(str(Decimal(x/5)), 1, BLACK)
+                                            number2 = font.render(str(Decimal(x/5)+12), 1, RED)
+                                    #indicate that the time is between 13 and 24
+                                    if DISPLAY == 24 and time_input >= 43200:
+                                            number = font.render(str(Decimal(x/5)), 1, RED)
+                                            number2 = font.render(str(Decimal((x/5)+12)), 1, BLACK)
+                                    #assume the user has it on a 12 hour cycle
+                                    elif DISPLAY == 12:
+                                            number = font.render(str(Decimal(x/5)), 1, BLACK)
+
+                            display.blit(number, ((center+x_coor) - 5, (center+y_coor) - 10))
+                            if DISPLAY == 24:
+                                    display.blit(number2, (center+(x_coor*.8) - 5, center+(y_coor*.8) - 10))
+
+                    #draw the lines between the numbers
                     else:
-                            #indicate that the time is between 0 and 12
-                            if DISPLAY == 24 and time_input < 43200:
-                                    number = font.render(str(Decimal(x/5)), 1, BLACK)
-                                    number2 = font.render(str(Decimal(x/5)+12), 1, RED)
-                            #indicate that the time is between 13 and 24
-                            if DISPLAY == 24 and time_input >= 43200:
-                                    number = font.render(str(Decimal(x/5)), 1, RED)
-                                    number2 = font.render(str(Decimal((x/5)+12)), 1, BLACK)
-                            #assume the user has it on a 12 hour cycle
-                            elif DISPLAY == 12:
-                                    number = font.render(str(Decimal(x/5)), 1, BLACK)
-
-                    display.blit(number, ((center+x_coor) - 5, (center+y_coor) - 10))
-                    if DISPLAY == 24:
-                            display.blit(number2, (center+(x_coor*.8) - 5, center+(y_coor*.8) - 10))
-
-            #draw the lines between the numbers
-            else:
-                 pygame.draw.line(display, BLACK, (center+(x_coor*.97),center+(y_coor*.97)), (center+(x_coor*1.03), center+(y_coor*1.03)), 1)
+                         pygame.draw.line(display, BLACK, (center+(x_coor*.97),center+(y_coor*.97)), (center+(x_coor*1.03), center+(y_coor*1.03)), 1)
 
 	#draw markers
 
@@ -118,7 +130,7 @@ while True:
 
                 display.blit(side, (center-10,center+25))
 
-	#drawing line
+	#drawing line for analog
 	pygame.draw.line(display, RED, (center, center), (center+x_sec, center+y_sec), 2)
 	pygame.draw.line(display, BLUE, (center, center), (center+x_min, center+y_min), 2)
 	pygame.draw.line(display, BLACK, (center, center), (center+x_hour, center+y_hour), 2)
