b01fd05fb4d42ca94139d33f6b479146ee0ecf4e
Diego Soliz <d481s306@ku.edu>
2016-02-11 10:22:41 -0600

    -Improved menu and user experience

--- main.py
+++ main.py
@@ -6,37 +6,45 @@ from menu_bar import *
 #input menu() waits for user to input valid time
 #in format 00 00 00 to then return and int
 
-time_input = input_menu()
-
 #time_input = feed_HMS.run()
 
-#change this value to select starting clock
-toggle = 2
+#toggle = 0 to start menu
+#toggle = 1 to draw analog clock
+#toggle = 2 to draw digital clock
+toggle = 0
 
 while True:
-	
+
 	#displaying background
 	bg = pygame.image.load("background.jpg")
 	display.blit(bg, (0, 0))
 	#uncoment to display white background
 	#display.fill(WHITE)
 
+	if time_input > 86400:
+		time_input = 0
+
 	if (toggle == 0):
-		#run menu
-		print ('toggle = 1')
+		toggle = runMenu()
+		if toggle == 0:
+			print ("Insert time using number keys on the pygame window (format hr 00 min 00 sec 00):")
+			time_input = input_menu()
 	elif (toggle == 1):
 		draw_analog_clock(time_input)
-	elif toggle == 2:
+	elif (toggle == 2):
 		draw_digital_clock(time_input)
-		
+
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
-                elif event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_a:
-                                toggle = 1
-                        elif event.key == pygame.K_d:
-                                toggle = 2
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_a:
+				toggle = 1
+			elif event.key == pygame.K_d:
+				toggle = 2
+			elif event.key == pygame.K_s:
+				toggle = 0
+
 	time_input += 1
 	time.sleep(1)
--- menu_bar.py
+++ menu_bar.py
@@ -10,37 +10,46 @@ font = pygame.font.Font(None, 25)
 number = font.render("12", 1, BLACK)
 
 def runMenu():
-	printMenu()
+	printMenu_console()
+	printMenu_display()
 	while True:
-		for event in pygame.event.get():		
+		for event in pygame.event.get():
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_a:
-					toggle = 1
-			
+					out_toggle = 1
+					return out_toggle
 				elif event.key == pygame.K_d:
-					toggle = 2
-				
+					out_toggle = 2
+					return out_toggle
 				elif event.key == pygame.K_s:
-					time_input = feed_HMS.run()
-					print "time set press x to load clock"
-					
+					out_toggle = 0
+					return out_toggle
+					print "input time"
 				elif event.key == pygame.K_m:
-					printMenu()
-				elif event.key == pygame.K_x:
-					return
-
-
-def printMenu():
+					printMenu_console()
+				elif event.key == pygame.K_SPACE:
+					print("need method to change to 24 hour mode")
+def printMenu_console():
 	print "Press a for analog clock display"
 	print "Press d for digital clock display"
+	print "press space to change between 24 or 12 hour mode"
 	print "Press s to set time"
 	print "Press m for menu options"
+
+def printMenu_display():
+	display.fill(WHITE)
+	my_list = ("Press a for analog clock display" , "Press d for digital clock display" , "Press space to select 24/12 hour mode" , "Press s to set time" , "Press m for menu options")
+	for i in range(len(my_list)):
+		message = font.render(my_list[i], 1, BLACK)	
+		display.blit(message, (0, i*30 + 30))
+	pygame.display.update()
+
 
 #method to accept numeric keyboard input on pygame window
 def input_menu():
 	input_string = ''
 	while True:
-		for event in pygame.event.get():		
+		for event in pygame.event.get():
 			if event.type == pygame.KEYDOWN:
 				#this gets input from pygame window and adds it to a string
 				if event.key in (pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9 ):
